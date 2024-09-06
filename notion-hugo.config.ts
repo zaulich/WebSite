0273ca0d9f13e4256e801f1d7e5172b5956fcbca
Mads Zaulich <mads@Madss-MacBook-Pro.local>
2024-09-06 08:55:08 +0200

    notion test

---
+++ notion-hugo.config.ts
@@ -0,0 +1,30 @@
+import { UserConfig } from "./src/config"
+
+const userConfig: UserConfig = {
+    mount: {
+        manual: false,
+        page_url: 'https://www.notion.so/zaulich/Notion-DoIt-4ca550ffc8e14eea8c213be8e21b4278?pvs=4',
+        pages: [
+            // {
+            //     page_id: '<page_id>',
+            //     target_folder: 'path/relative/to/content/folder'
+            // }
+            {
+                page_id: '45eb121158b9489480ec000fd25c812b',
+                target_folder: '.'
+            }
+        ],
+        databases: [
+            // {
+            //     database_id: '<database_id>',
+            //     target_folder: 'path/relative/to/content/folder'
+            // }
+            {
+                database_id: 'b7b1816c05ec464391c8c111fa242985',
+                target_folder: '.'
+            }
+        ],
+    }
+}
+
+export default userConfig;
